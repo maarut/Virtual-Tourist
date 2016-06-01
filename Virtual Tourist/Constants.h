da3323bf6d8934d4d781cabb00f9c9d7f57acbdf
Maarut Chandegra <contact@maarut.net>
2016-06-01 17:54:58 +0100

    Add files to store Flickr API keys

---
+++ Virtual Tourist/Constants.h
@@ -0,0 +1,12 @@
+//
+//  Constants.h
+//  Virtual Tourist
+//
+//  Created by Maarut Chandegra on 01/06/2016.
+//  Copyright © 2016 Maarut Chandegra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const kFlickrAPIKey;
+extern NSString *const kFlickrAPISecret;
---
+++ Virtual Tourist/Constants.m
@@ -0,0 +1,12 @@
+//
+//  Constants.m
+//  Virtual Tourist
+//
+//  Created by Maarut Chandegra on 01/06/2016.
+//  Copyright © 2016 Maarut Chandegra. All rights reserved.
+//
+
+#import "Constants.h"
+
+NSString *const kFlickrAPIKey = FLICKR_API_KEY;
+NSString *const kFlickrAPISecret = FLICKR_API_SECRET;
